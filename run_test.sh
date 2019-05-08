20fd6179abf2f815387c3873cae9e1dafd123abf
Shin T <shintaro.toita@ipmu.jp>
2019-05-08 23:43:04 +0900

    move run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 # Run this from the top level directory
-
 rm -f test/test.out
-./build/sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s test/test/ > /dev/null
+sdpb --precision=1024 --noFinalCheckpoint --procsPerNode=1 -s "test/test/"
 diff test/test.out test/test.out.orig
 if [ $? == 0 ]
 then
@@ -13,4 +11,3 @@ else
     echo "FAIL"
     exit 1
 fi
-    
